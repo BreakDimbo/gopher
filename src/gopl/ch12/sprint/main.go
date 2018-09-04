9b3735117662c3262f09abcfee07a0713a7bfadc
break.D <dhpigtc@gmail.com>
2018-09-04 23:32:27 +0800

    chapter 12

---
+++ src/gopl/ch12/sprint/main.go
@@ -0,0 +1,54 @@
+package format
+
+import (
+	"reflect"
+	"strconv"
+)
+
+func Sprint(x interface{}) string {
+	type stringer interface {
+		String() string
+	}
+
+	switch x := x.(type) {
+	case stringer:
+		return x.String()
+	case string:
+		return x
+	case int:
+		return strconv.Itoa(x)
+	case bool:
+		if x {
+			return "true"
+		}
+		return "false"
+	default:
+		return "???"
+	}
+}
+
+func Any(value interface{}) string {
+	return formatAtom(reflect.ValueOf(value))
+}
+
+func formatAtom(v reflect.Value) string {
+	switch v.Kind() {
+	case reflect.Invalid:
+		return "invalid"
+	case reflect.Int, reflect.Int8, reflect.Int16,
+		reflect.Int32, reflect.Int64:
+		return strconv.FormatInt(v.Int(), 10)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16,
+		reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return strconv.FormatUint(v.Uint(), 10)
+	case reflect.Bool:
+		return strconv.FormatBool(v.Bool())
+	case reflect.String:
+		return strconv.Quote(v.String())
+	case reflect.Chan, reflect.Func, reflect.Map, reflect.Slice, reflect.Ptr:
+		return v.Type().String() + "0x" +
+			strconv.FormatUint(uint64(v.Pointer()), 16)
+	default: // reflect.Array, reflect.Struct, reflect.Interface:
+		return v.Type().String() + " value"
+	}
+}
